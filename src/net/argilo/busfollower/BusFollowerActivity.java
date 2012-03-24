5d74c57e2ebf911b4cb0057ee10521041269eb1a
Clayton Smith <argilo@gmail.com>
2012-03-24 17:33:42 -0400

    Zoom to OC Transpo service area at start.

--- src/net/argilo/busfollower/BusFollowerActivity.java
+++ src/net/argilo/busfollower/BusFollowerActivity.java
@@ -8,6 +8,7 @@ import net.argilo.busfollower.ocdata.Trip;
 
 import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
 import com.google.android.maps.Overlay;
 import com.google.android.maps.OverlayItem;
@@ -19,6 +20,12 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class BusFollowerActivity extends MapActivity {
+	// Values taken from stops.txt.
+	private static int minLatitude = 45130104; 
+	private static int maxLatitude = 45519650;
+	private static int minLongitude = -76040543;
+	private static int maxLongitude = -75342690;
+	
 	private OCTranspoDataFetcher dataFetcher;
 	
     /** Called when the activity is first created. */
@@ -33,6 +40,11 @@ public class BusFollowerActivity extends MapActivity {
         
         final MapView mapView = (MapView) findViewById(R.id.mapView);
         mapView.setBuiltInZoomControls(true);
+        
+        // Zoom to OC Transpo service area at start.
+        MapController mapController = mapView.getController();
+        mapController.zoomToSpan((maxLatitude - minLatitude), (maxLongitude - minLongitude));
+        mapController.setCenter(new GeoPoint((maxLatitude + minLatitude) / 2, (maxLongitude + minLongitude) / 2));
         
         final Button updateButton = (Button) findViewById(R.id.updateButton);
         updateButton.setOnClickListener(new View.OnClickListener() {
