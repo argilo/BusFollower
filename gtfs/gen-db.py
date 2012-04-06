06157d675a5e7bf4bf57165e644061dd18e5dda6
Clayton Smith <argilo@gmail.com>
2012-04-06 19:30:20 -0400

    Use transitfeed to do the GTFS parsing.

--- gtfs/gen-db.py
+++ gtfs/gen-db.py
@@ -1,13 +1,9 @@
 # coding=utf-8
 
+import transitfeed
 import os
 import sqlite3
-import csv
 import re
-
-def stringToMicroDegrees(degreesString):
-    degreesString = degreesString.strip()
-    return int(0.5 + (1000000.0 * float(degreesString)))
 
 def normalizeStopCode(stopCode):
     if re.match(r"^\d\d\d\d$", stopCode):
@@ -16,23 +12,23 @@ def normalizeStopCode(stopCode):
         return None
 
 def normalizeStopName(stopName):
-    stopName = stopName.replace("''", "'")
-    stopName = stopName.replace("À", "A")
-    stopName = stopName.replace("Â", "A")
-    stopName = stopName.replace("Æ", "AE")
-    stopName = stopName.replace("Ç", "C")
-    stopName = stopName.replace("È", "E")
-    stopName = stopName.replace("É", "E")
-    stopName = stopName.replace("Ê", "E")
-    stopName = stopName.replace("Ë", "E")
-    stopName = stopName.replace("Î", "I")
-    stopName = stopName.replace("Ï", "I")
-    stopName = stopName.replace("Ô", "O")
-    stopName = stopName.replace("Ö", "O")
-    stopName = stopName.replace("Ù", "U")
-    stopName = stopName.replace("Û", "U")
-    stopName = stopName.replace("Ü", "U")
-    stopName = stopName.replace("Œ", "OE")
+    stopName = stopName.replace(u"''", "'")
+    stopName = stopName.replace(u"À", "A")
+    stopName = stopName.replace(u"Â", "A")
+    stopName = stopName.replace(u"Æ", "AE")
+    stopName = stopName.replace(u"Ç", "C")
+    stopName = stopName.replace(u"È", "E")
+    stopName = stopName.replace(u"É", "E")
+    stopName = stopName.replace(u"Ê", "E")
+    stopName = stopName.replace(u"Ë", "E")
+    stopName = stopName.replace(u"Î", "I")
+    stopName = stopName.replace(u"Ï", "I")
+    stopName = stopName.replace(u"Ô", "O")
+    stopName = stopName.replace(u"Ö", "O")
+    stopName = stopName.replace(u"Ù", "U")
+    stopName = stopName.replace(u"Û", "U")
+    stopName = stopName.replace(u"Ü", "U")
+    stopName = stopName.replace(u"Œ", "OE")
     return stopName
 
 conn = sqlite3.connect(os.path.join('assets','db'))
@@ -52,23 +48,16 @@ stop_lon INT
 )''')
 c.execute('CREATE INDEX stop_code ON stops(stop_code)')
 
-with open(os.path.join('gtfs','stops.txt')) as f:
-    reader = csv.reader(f)
-    columns = reader.next()
+loader = transitfeed.Loader(os.path.join('gtfs','google_transit.zip'))
+schedule = loader.Load()
 
-    stopIdCol = columns.index('stop_id')
-    stopCodeCol = columns.index('stop_code')
-    stopNameCol = columns.index('stop_name')
-    stopLatCol = columns.index('stop_lat')
-    stopLonCol = columns.index('stop_lon')
-    
-    for row in reader:
-        values = [row[stopIdCol], \
-                  normalizeStopCode(row[stopCodeCol]), \
-                  normalizeStopName(row[stopNameCol]), \
-                  stringToMicroDegrees(row[stopLatCol]), \
-                  stringToMicroDegrees(row[stopLonCol])]
-        c.execute('INSERT INTO stops VALUES (?,?,?,?,?)', values)
+for stop_id, stop in schedule.stops.items():
+    values = [stop.stop_id, \
+              normalizeStopCode(stop.stop_code), \
+              normalizeStopName(stop.stop_name), \
+              int(0.5 + 1000000 * stop.stop_lat), \
+              int(0.5 + 1000000 * stop.stop_lon)]
+    c.execute('INSERT INTO stops VALUES (?,?,?,?,?)', values)
 
 conn.commit()
 c.close()
