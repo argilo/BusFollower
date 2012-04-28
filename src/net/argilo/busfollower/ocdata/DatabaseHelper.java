8bb3e6d2044b2962db220b60f166fb57f5d471e3
Clayton Smith <argilo@gmail.com>
2012-04-28 11:18:30 -0400

    Increment database version so spring 2012 data will be picked up.

--- src/net/argilo/busfollower/ocdata/DatabaseHelper.java
+++ src/net/argilo/busfollower/ocdata/DatabaseHelper.java
@@ -17,7 +17,7 @@ public class DatabaseHelper {
 	private static final String DATABASE_FOLDER = "/data/data/net.argilo.busfollower/databases";
 	private static final String DATABASE_PATH = DATABASE_FOLDER + "/db";
 	private static final String DATABASE_PREFS = "DbPrefsFile";
-	private static final int DATABASE_VERSION = 6; // Increment this whenever the DB is changed
+	private static final int DATABASE_VERSION = 7; // Increment this whenever the DB is changed
 
 	Context context;
 	
