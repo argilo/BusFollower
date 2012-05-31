86bd7f48f8cfd9e5b8fc73c560adac69283dce28
Clayton Smith <argilo@gmail.com>
2012-05-31 18:39:26 -0400

    Add GPL to all Java files.

--- src/net/argilo/busfollower/Util.java
+++ src/net/argilo/busfollower/Util.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright 2012 Clayton Smith
+ *
+ * This file is part of Ottawa Bus Follower.
+ *
+ * Ottawa Bus Follower is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3, or (at
+ * your option) any later version.
+ *
+ * Ottawa Bus Follower is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Ottawa Bus Follower; see the file COPYING.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
 package net.argilo.busfollower;
 
 import java.lang.reflect.Method;
