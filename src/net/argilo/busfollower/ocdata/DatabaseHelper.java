b449269c338de87a0207058f94715e75f672d022
Clayton Smith <argilo@gmail.com>
2014-12-30 13:39:18 -0500

    Pick up the latest OC Transpo GTFS data.

--- src/net/argilo/busfollower/ocdata/DatabaseHelper.java
+++ src/net/argilo/busfollower/ocdata/DatabaseHelper.java
@@ -35,7 +35,7 @@ import android.util.Log;
 public class DatabaseHelper {
     private static final String TAG = "DatabaseHelper";
     private static final String DATABASE_PREFS = "DbPrefsFile";
-    private static final int DATABASE_VERSION = 14; // Increment this whenever the DB is changed
+    private static final int DATABASE_VERSION = 15; // Increment this whenever the DB is changed
 
     Context context;
     private final String DATABASE_FOLDER;
