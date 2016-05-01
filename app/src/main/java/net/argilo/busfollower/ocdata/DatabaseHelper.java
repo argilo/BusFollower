05d9e48b6c857e9dcd6d705053b0f1ab7f7889cc
Clayton Smith <argilo@gmail.com>
2016-05-01 17:21:49 -0400

    Update with latest OC Transpo data.

--- app/src/main/java/net/argilo/busfollower/ocdata/DatabaseHelper.java
+++ app/src/main/java/net/argilo/busfollower/ocdata/DatabaseHelper.java
@@ -35,7 +35,7 @@ import android.util.Log;
 public class DatabaseHelper {
     private static final String TAG = "DatabaseHelper";
     private static final String DATABASE_PREFS = "DbPrefsFile";
-    private static final int DATABASE_VERSION = 21; // Increment this whenever the DB is changed
+    private static final int DATABASE_VERSION = 22; // Increment this whenever the DB is changed
 
     private Context context;
     private final String DATABASE_FOLDER;
