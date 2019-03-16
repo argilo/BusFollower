fa7802d7e7bd9e53abd3b7ac110129334faf6f68
Clayton Smith <argilo@gmail.com>
2019-03-16 14:02:07 -0400

    Migrate to AndroidX.

--- app/src/main/java/net/argilo/busfollower/StopChooserActivity.java
+++ app/src/main/java/net/argilo/busfollower/StopChooserActivity.java
@@ -31,7 +31,7 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.KeyEvent;
