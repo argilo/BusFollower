b746c3848fd38c8e67800efe31c9285d0ec3c251
Clayton Smith <argilo@gmail.com>
2021-09-04 09:20:36 -0400

    Correct another OC Transpo typo

--- gtfs/gen_db.py
+++ gtfs/gen_db.py
@@ -78,6 +78,7 @@ def normalize_stop_name(stop_name):
     stop_name = stop_name.replace("MER BELUE", "MER BLEUE")
     stop_name = stop_name.replace("ROCKLAND / BUCKLAND", "ROCKDALE / BUCKLAND")
     stop_name = stop_name.replace("KITTIWKE", "KITTIWAKE")
+    stop_name = stop_name.replace("CHARLEMANGNE", "CHARLEMAGNE")
     stop_name = stop_name.replace("/", " / ")
     stop_name = " ".join(stop_name.split())
     stop_name = re.sub(r"\b(STE?)-", r"\1 ", stop_name)
